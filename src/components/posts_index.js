5953dc05aa4f653d6f5fef3e19150fb00f927db1
benjaminhenriquez <henriquez.ben@gmail.com>
2017-03-26 22:49:03 -0400

    finished blog

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {fetchPosts} from '../actions/index'
+import {fetchPosts} from '../actions/index';
+import { Link } from 'react-router';
+
 
 class PostsIndex extends Component {
   componentWillMount(){
     this.props.fetchPosts();
   }
 
+  renderPosts(){
+    return this.props.posts.map((post) => {
+      return(
+        <li className="list-group-item" key={post.id}>
+        <Link to={"posts/" + post.id}>
+        <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    })
+  }
+
   render(){
     return (
-      <div> List of blog posts </div>
+      <div>
+        <div className="text-xs-right">
+        <Link to="/posts/new" className="btn btn-primary">
+        Add a post</Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+        {this.renderPosts()}
+        </ul>
+      </div>
     )
   }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({fetchPosts}, dispatch);
+function mapStateToProps(state){
+  return {posts: state.posts.all};
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import posts from './reducer_posts';
+import PostsReducer from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
